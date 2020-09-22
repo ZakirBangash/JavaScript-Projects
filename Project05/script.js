98ad598028af035a08ca50d54beb544654a01209
ZakirBangash <engrzakirbangash@gmail.com>
2020-09-22 23:26:46 +0500

    script.js
    Completed all the javaScript work

--- Project05/script.js
+++ Project05/script.js
@@ -12,24 +12,24 @@ generateRandomUser();
 generateRandomUser();
 
 // Function to fetch random user form api
-async function generateRandomUser(){
+async function generateRandomUser() {
     const response = await fetch('https://randomuser.me/api');
-    const {results} = await response.json();
-    let {first,last} = results[0].name;
-    
+    const { results } = await response.json();
+    let { first, last } = results[0].name;
+
     const newUser = {
-        name:`${first} ${last}`,
+        name: `${first} ${last}`,
         worth: Math.floor(Math.random() * 1000000)
     };
 
     addData(newUser);
-    
+
 }
 
 
-function addData(newUser){
-   data.push(newUser);
-   updateDom();
+function addData(newUser) {
+    data.push(newUser);
+    updateDom();
 }
 
 
@@ -42,16 +42,68 @@ function formatCurrency(num) {
 
 
 // Function to update UI with updateDOM
-function updateDom(inputData=data){
+function updateDom(inputData = data) {
     main.innerHTML = `<h2><strong>Name</strong> Net Worth</h2>`;
-    
+
     inputData.forEach(item => {
         const element = document.createElement('div');
         element.classList.add('name');
         element.innerHTML = `<strong>${item.name}</strong> ${formatCurrency(item.worth)}`;
         console.log(element);
-        main.appendChild(element);        
+        main.appendChild(element);
     });
 
 }
 
+
+// Function of Double the Worth
+function doubleWorth() {
+    data = data.map(item => {
+
+        return { ...item, worth: item.worth * 2 }
+    })
+    updateDom();
+}
+
+// Function to show millionaires
+function showMillionaires() {
+    data = data.filter(item => item.worth > 1000000);
+    updateDom();
+}
+
+// Function to Sort by Richest man
+function sortRichest() {
+    data.sort((a, b) => {
+        return a.worth - b.worth;
+    })
+
+    updateDom();
+}
+
+// Function to calculate total net worth of all user 
+function calculateTotalNetWorth() {
+    const totalWorth = data.reduce((acc, item) => (acc += item.worth), 0);
+    
+    const totalNetWorthElement = document.createElement('div');
+    totalNetWorthElement.innerHTML = `<h3>Total Net Worth: <strong>${formatCurrency(totalWorth)}</strong></h3>`;
+    main.appendChild(totalNetWorthElement)
+
+}
+
+
+// Event Listners
+
+// 1. add user event listner
+addUser.addEventListener('click', generateRandomUser);
+
+// 2. add double money event listner
+double.addEventListener('click', doubleWorth);
+
+// 3. add Show event listner
+millionaries.addEventListener('click', showMillionaires);
+
+// 4. add Sort event listner
+sort.addEventListener('click', sortRichest);
+
+//5. add total event listner
+total.addEventListener('click', calculateTotalNetWorth)
